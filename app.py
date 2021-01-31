e2366d1a35ef5e0dbf3cf6b2d8ac5e79f73fdb02
frknsgt <furkan.v.sk@gmail.com>
2021-01-31 19:13:47 +0300

    fix: faulty file path and page designs revised

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template,request
 import joblib
+import numpy as np
 
 app = Flask(__name__)
 
@@ -11,15 +12,14 @@ def home():
 @app.route("/tahmin", methods = ["POST"])
 
 def tahmin_et():
-
     model = open("Earthquake_model.pkl","rb")
     clf = joblib.load(model)
     if request.method == "POST":
         koordinat1 = request.form["koordinat1"]
         koordinat2 = request.form["koordinat2"]
-        data = [[koordinat1,koordinat2]]
+        data = [np.array([koordinat1,koordinat2])]
         prediction = clf.predict(data)
-    return render_template("result.html", prediction)
+    return render_template("result.html", data=prediction[0])
 
 if __name__ == '__main__':
     app.run()
